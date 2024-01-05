6159a81fdd0555854d49645cd31d7bd254a1308a
Wahyu Widhi Dyatmika <146448699+wwdhd@users.noreply.github.com>
2024-01-05 04:57:03 +0000

    Update code1_Rev_parallel.f90

--- code1_Rev_parallel.f90
+++ code1_Rev_parallel.f90
@@ -1,9 +1,7 @@
 PROGRAM LINEAR1
 use MPI
 !include 'mpif.h'
-
 IMPLICIT NONE
-
 !!!!DECLARATION OF VARIABLES!!!!
 REAL :: LB     ! left boundary LOCATION
 REAL :: RB     ! RIGHT BOUNDARY LOCATION
@@ -39,8 +37,6 @@ INTEGER :: t_start_computation, t_end_computation, t_computation
 	CALL MPI_INIT(ierr)
       CALL MPI_COMM_RANK(MPI_COMM_WORLD, my_rank, ierr)
       CALL MPI_COMM_SIZE(MPI_COMM_WORLD, n_cpus, ierr)
-
-
 open(30,file="input.dat", form="formatted",action="read")
 read(30,*)
 read(30,*)POINTS
@@ -49,21 +45,14 @@ read(30,*)cfl
 read(30,*)method
  close(30)
 call mpi_barrier(MPI_COMM_WORLD, ierr)
-
-
 pi=4.0*atan(1.0d0)
 LB=-10.0
 RB=10.0
 u=1
 LENGTH=ABS(RB)+ABS(LB)
-
-
 DX=LENGTH/(POINTS-1)
 dt=cfl*dx/u
 parts=points/n_cpus
-
-
-
 istart=(my_rank*parts)+1
 iend=(my_rank+1)*parts
 
@@ -72,33 +61,18 @@ if (my_rank.eq.n_cpus-1)then
 end if
 print*,"here",istart,iend,my_rank
 call mpi_barrier(MPI_COMM_WORLD, ierr)
-
-
 !!!MEMORY ALLOCATION
-
 ALLOCATE(X(istart:iend)); X=0.0D0
 ALLOCATE(FI(istart-2:iend+2,1:2)); FI=0.0D0
-
-
-
 DO I=istart,iend
-
-X(I)=lb+(DX*I)
-
+	X(I)=lb+(DX*I)
 END DO
-
 !COORDINATES ALREADY SETUP
-
-
-
 DO I=istart,iend
 FI(I, 1) = SIN(2.0 * pi * X(I))
 END DO
-
-
 WRITE(PROC3,FMT='(I10)') my_rank
 OUTFILE="OUT_"//TRIM(ADJUSTL(PROC3))//".dat"
-
 open(30,file=outfile, form="formatted",status="replace")
 DO I=istart,iend
 WRITE(30,*)X(I), FI(I,1)
@@ -106,17 +80,12 @@ END DO
  close(30)
 call mpi_barrier(MPI_COMM_WORLD, ierr)
 stop
-
-
 T_C=0.0		!current time
 it=0
-
 do 	!start time loop
-
 	dt=min(dt,t_end-t_c)
 	! Start timer for boundary exchange
     	t_start_exchange = MPI_Wtime()
-	
 	!Multiprocessing start
 	!Rank 0
 	if(my_rank == 0) then
@@ -127,7 +96,6 @@ do 	!start time loop
 		call MPI_RECV(sentinfo0, 1, MPI_COMPLEX, 1, 2, MPI_COMM_WORLD, status14, ierr) !Rec from 1
 		FI(iend+1,1)=sentinfo0
 	end if
-
 	!Rank between 0 and ncp-1
 	if(my_rank > 0 .and. my_rank < n_cpus-1) then
 		call MPI_RECV(sentinfo1, 1, MPI_COMPLEX, my_rank-1, 1, MPI_COMM_WORLD, status14, ierr) !Request to left
@@ -139,7 +107,6 @@ do 	!start time loop
 		call MPI_RECV(sentinfo1, 1, MPI_COMPLEX, my_rank+1, 2, MPI_COMM_WORLD, status14, ierr) !Rec from right
 		FI(iend+1,1) = sentinfo1
 	end if
-
 	!If rank=ncp-1
 	if(my_rank == n_cpus-1) then
 		x(iend+1) = 10.0d0
@@ -152,25 +119,18 @@ do 	!start time loop
 	! End timer for boundary exchange
     	t_end_exchange = MPI_Wtime()	
 	t_exchange = t_end_exchange - t_start_exchange
-
 	call mpi_barrier(MPI_COMM_WORLD, ierr)
 	! Start timer for time-step computation
     	t_start_computation = MPI_Wtime()
-
 	!FTCS scheme
 	do i=istart, iend-1
 		fi(i,2)=fi(i,1)-(0.5*U*dt/dx)*(fi(i+1,1)-fi(i-1,1))
 	end do
 	fi(istart,2)=fi(iend-1,2)
-
   	! Current time update
   	t_c = t_c + dt
-
   	! Update solution (next time level copied to current)
   	Fi(istart:iend,1) = fi(istart:iend,2) 
-
-	
-
 	if (t_c.ge.t_end)then
 	! End timer for time-step computation
     	t_end_computation = MPI_Wtime()
@@ -182,30 +142,19 @@ do 	!start time loop
 	print*, X, FI
 		exit		!exit loop
 	end if
-
 end do
-
 open(unit=31, file='output.dat', status="replace")
 write(31, *) 'Time spent in boundary exchange: ', t_exchange
 write(31, *) 'Time spent in time-step computation: ', t_computation
  close(31)
-
-
 !write final solution
 WRITE(PROC3,FMT='(I10)') my_rank
 OUTFILE="OUT_"//TRIM(ADJUSTL(PROC3))//".dat"
 open(30,file=outfile, form="formatted",status="replace")
-
 DO I=istart-1,iend+1
 WRITE(30,*)X(I), FI(I,2)
 END DO
  close(30)
 call mpi_barrier(MPI_COMM_WORLD, ierr)
 call mpi_finalize(ierr)
-
-
-
-
-
-
 END PROGRAM LINEAR1
